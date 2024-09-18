78d584383d30c9290b61c803b1e3f347b1f36fa1
Jorge Robles Calvo <jrc00083@red.ujaen.es>
2024-09-18 11:43:52 +0200

    esquema modelo

---
+++ Model.cpp
@@ -0,0 +1,8 @@
+//
+// Created by DeJorgiK on 18/09/2024.
+//
+
+#include "Model.h"
+
+namespace PAG {
+} // PAG
--- Renderer.cpp
+++ Renderer.cpp
@@ -2,13 +2,8 @@
 
 namespace PAG {
     void Renderer::referescarVentana(GLFWwindow *window) {
-        // - Borra los buffers (color y profundidad)
         glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-        // - GLFW usa un doble buffer para que no haya parpadeo. Esta orden
-        // intercambia el buffer back (en el que se ha estado dibujando) por el
-        // que se mostraba hasta ahora (front).
-        glfwSwapBuffers(window); //OJO TIENE QUE SER LA ULTIMA ORDEN EN EL CALLBACK
-        std::cout << "REFRESH CALLBACK CALLED" << std::endl;
+        glfwSwapBuffers(window);
     }
 
     Renderer::Renderer() {}
